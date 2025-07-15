405d54bbdb755bc03f12d78f1db9cb86de919058
pumbas600 <joshjeffers600@gmail.com>
2025-07-15 20:53:46 +1200

    test: extract format code into helper function

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,23 +2,22 @@ import * as prettier from "prettier";
 import prettierPluginLess from "../src/index";
 import { describe, expect, test } from "vitest";
 
-const code = `
-.mixin-double(@value) {
-  @return: (@value * 2);
-}
-
-.my-class {
-  font-size: .mixin-double(16px)[];
-}
-`;
-
 describe("prettierPluginLess", () => {
-  test("returns code without adding space between mixin and []", async () => {
-    const formattedCode = await prettier.format(code, {
+  const formatWithPlugin = async (code: string): Promise<string> => {
+    return await prettier.format(code, {
       parser: "less",
       plugins: [prettierPluginLess],
     });
+  };
 
+  test("returns code without adding space between mixin and []", async () => {
+    const code = `
+    .my-class {
+      font-size: .mixin-double(16px)[];
+    }
+    `;
+
+    const formattedCode = await formatWithPlugin(code);
     expect(formattedCode).toBe(code);
   });
 });
