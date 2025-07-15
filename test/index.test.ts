e33f14cfe4a28e2a1d6b1e5f64c5d36a0c8db7eb
pumbas600 <joshjeffers600@gmail.com>
2025-07-15 22:30:05 +1200

    test: add more unit tests (and discover that it breaks with value in brackets)

--- test/index.test.ts
+++ test/index.test.ts
@@ -21,4 +21,66 @@ describe("prettierPluginLess", () => {
     const formattedCode = await formatWithPlugin(code);
     expect(formattedCode).toBe(code);
   });
+
+  test("returns code with space between mixin and [] removed", async () => {
+    const code = [
+      ".my-class {",
+      "  font-size: .mixin-double(16px) [];",
+      "}",
+      "",
+    ].join("\n");
+
+    const expectedCode = [
+      ".my-class {",
+      "  font-size: .mixin-double(16px)[];",
+      "}",
+      "",
+    ].join("\n");
+
+    const formattedCode = await formatWithPlugin(code);
+    expect(formattedCode).toBe(expectedCode);
+  });
+
+  test("return code without adding space between mixin without parameters and []", async () => {
+    const code = [
+      ".my-class {",
+      "  font-size: .mixin-double()[];",
+      "}",
+      "",
+    ].join("\n");
+
+    const formattedCode = await formatWithPlugin(code);
+    expect(formattedCode).toBe(code);
+  });
+
+  test("returns code with space between mixin without parameters and [] removed", async () => {
+    const code = [
+      ".my-class {",
+      "  font-size: .mixin-double() [];",
+      "}",
+      "",
+    ].join("\n");
+
+    const expectedCode = [
+      ".my-class {",
+      "  font-size: .mixin-double()[];",
+      "}",
+      "",
+    ].join("\n");
+
+    const formattedCode = await formatWithPlugin(code);
+    expect(formattedCode).toBe(expectedCode);
+  });
+
+  test("returns code without adding space between mixing and [@return]", async () => {
+    const code = [
+      ".my-class {",
+      "  font-size: .mixin-double(16px)[@return];",
+      "}",
+      "",
+    ].join("\n");
+
+    const formattedCode = await formatWithPlugin(code);
+    expect(formattedCode).toBe(code);
+  });
 });
