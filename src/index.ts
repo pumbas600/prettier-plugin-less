24ffa311e8c1ef58cf1c864075dc94cedb9ac049
pumbas600 <joshjeffers600@gmail.com>
2025-07-15 20:50:35 +1200

    fix: get custom print function to run

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import { printers } from "prettier/plugins/postcss";
-import { Options, Plugin } from "prettier/";
+import { parsers, printers } from "prettier/plugins/postcss";
+import { Plugin } from "prettier";
 
-const options: Options = {};
+const AST_FORMAT = "prettier-plugin-less-ast";
 
 const prettierPluginLess: Plugin = {
+  parsers: {
+    less: {
+      ...parsers.less,
+      astFormat: AST_FORMAT,
+    },
+  },
   printers: {
-    postcss: {
+    [AST_FORMAT]: {
       ...printers.postcss,
       print: (...args) => {
         return printers.postcss.print(...args);
